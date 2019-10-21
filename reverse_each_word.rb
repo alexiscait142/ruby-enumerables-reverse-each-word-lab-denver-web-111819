7383702b6bc0139d72d7d22bba37405ffdc51872
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-21 03:19:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
 def reverse_each_word(array)
-  array.split(" ").collect {|word| word.reverse!}.join(" ")
+  new_array = array.split(" ")
+  reverse_array = new_array.collect{|word| word.reverse}
+  joined_array = reverse_array.join(" ")
+  joined_array
 end
